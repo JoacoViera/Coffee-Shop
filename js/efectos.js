da344cab54a3b0e5839e78c91fce7f28d1fc3fba
Joaquín Viera <joaquin.viera@outlook.com>
2019-01-05 04:39:33 -0300

    Dirrecion de menu
    Arreglos porque en dispositivos moviles no iba a donde debia

--- js/efectos.js
+++ js/efectos.js
@@ -12,6 +12,11 @@ $(document).ready(function () {
 
     //Efecto Header
 
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
     if($(window).width() > 1000){
         $('header .textos').css({
             opacity: 0,
@@ -22,41 +27,70 @@ $(document).ready(function () {
             opacity: 1,
             marginTop: '-52 px'
         }, 1500);
+
+        //Scroll
+        
+        $('#btn-acerca-de').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: acercaDe -100
+            }, 500);
+        });
+
+        $('#btn-menu').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: menu
+            }, 500);
+        });
+
+        $('#btn-galeria').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: galeria
+            }, 500);
+        });
+
+        $('#btn-ubicacion').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: ubicacion +100
+            }, 500);
+        });
+    } else{
+        $('#btn-acerca-de').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: acercaDe 
+            }, 500);
+        });
+
+        $('#btn-menu').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: menu
+            }, 500);
+        });
+
+        $('#btn-galeria').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: galeria
+            }, 500);
+        });
+
+        $('#btn-ubicacion').on('click',function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: ubicacion 
+            }, 500);
+        });
+
     }
+    
 
-    //Scroll
-    var acercaDe = $('#acerca-de').offset().top,
-        menu = $('#platillos').offset().top,
-        galeria = $('#galeria').offset().top,
-        ubicacion = $('#ubicacion').offset().top;
-
-    $('#btn-acerca-de').on('click',function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: acercaDe -100
-        }, 500);
-    });
-
-    $('#btn-menu').on('click',function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: menu
-        }, 500);
-    });
-
-    $('#btn-galeria').on('click',function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: galeria
-        }, 500);
-    });
-
-    $('#btn-ubicacion').on('click',function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: ubicacion +100
-        }, 500);
-    });
+  
+    
 
 
 
